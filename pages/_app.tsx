6ec70c2259d57eb53e8f73d802373320a348120f
Samuel Lee <samuelkyuseo@gmail.com>
2023-02-11 19:11:30 -0800

    hellow world app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css';
+import '../styles/global.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
