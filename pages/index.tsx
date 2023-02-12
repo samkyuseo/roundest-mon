d10ac679d9e9990e7b406218935f4eeefacabcc4
Samuel Lee <samuelkyuseo@gmail.com>
2023-02-11 19:28:22 -0800

    basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 export default function Home() {
   return (
-    <div className='h-screen w-screen flex flex-col'>
-      <div className=''>Hello world</div>
-      <div className=''></div>
+    <div className="h-screen w-screen flex flex-col justify-center align-middle items-center">
+      <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200">Option 1</div>
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-400">Option 1</div>
+      </div>
     </div>
   );
 }
