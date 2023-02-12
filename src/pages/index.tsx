72a464a262849d17d078b25238c1dfce42b47e43
Samuel Lee <samuelkyuseo@gmail.com>
2023-02-12 09:57:44 -0800

    added pokemons title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,25 +12,30 @@ export default function Home() {
     return <div>Loading...</div>;
   }
 
-  // console.log(data);
   return (
     <div className="h-screen w-screen flex flex-col justify-center align-middle items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-40 h-40">
+        <div className="w-64 h-64 flex flex-col">
           <img
-            className="w-full h-full"
-            src={firstPokemon.data?.sprites.front_default}
+            className="h-full"
+            src={firstPokemon.data?.sprites.front_default as string}
           />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {firstPokemon.data?.name}
+          </div>
         </div>
         <div className="p-8">Vs</div>
-        <div className="w-40 h-40">
+        <div className="w-64 h-64 flex flex-col">
           {" "}
           <img
-            className="w-full h-full"
-            src={secondPokemon.data?.sprites.front_default}
+            className="h-full"
+            src={secondPokemon.data?.sprites.front_default as string}
           />
+          <div className="text-xl text-center capitalize mt-[-2rem]">
+            {secondPokemon.data?.name}
+          </div>
         </div>
       </div>
     </div>
