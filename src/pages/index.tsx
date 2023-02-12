6858a86a174dd341bfe5175f22896b84fbece341
Samuel Lee <samuelkyuseo@gmail.com>
2023-02-12 10:59:55 -0800

    add buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,12 +12,21 @@ export default function Home() {
     return <div>Loading...</div>;
   }
 
+  const voteForRoundest = (selected: number) => {
+    // todo: fire mutation to persist changes
+
+    updateIds(getOptionsForVote());
+  };
+
+  const btn =
+    "bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow";
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center align-middle items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-64 h-64 flex flex-col">
+        <div className="w-64 h-64 flex flex-col items-center">
           <img
             className="h-full"
             src={firstPokemon.data?.sprites.front_default as string}
@@ -25,10 +34,12 @@ export default function Home() {
           <div className="text-xl text-center capitalize mt-[-2rem]">
             {firstPokemon.data?.name}
           </div>
+          <button className={btn} onClick={() => voteForRoundest(first)}>
+            Rounder
+          </button>
         </div>
         <div className="p-8">Vs</div>
-        <div className="w-64 h-64 flex flex-col">
-          {" "}
+        <div className="w-64 h-64 flex flex-col items-center">
           <img
             className="h-full"
             src={secondPokemon.data?.sprites.front_default as string}
@@ -36,6 +47,9 @@ export default function Home() {
           <div className="text-xl text-center capitalize mt-[-2rem]">
             {secondPokemon.data?.name}
           </div>
+          <button className={btn} onClick={() => voteForRoundest(second)}>
+            Rounder
+          </button>
         </div>
       </div>
     </div>
