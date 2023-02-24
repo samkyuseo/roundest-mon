4b7a2f46a5a87131d154acd7c19929800feeda87
Samuel Lee <samuelkyuseo@gmail.com>
2023-02-23 19:49:09 -0800

    Add prisma and basic endpoint for voting

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 import { procedure, router } from "../trpc";
 import { PokemonClient } from "pokenode-ts";
+import { prisma } from "@/server/utils/prisma";
+
 export const appRouter = router({
   hello: procedure
     .input(
@@ -14,15 +16,21 @@ export const appRouter = router({
       };
     }),
   "get-pokemon-by-id": procedure
-    .input(
-      z.object({
-        id: z.number(),
-      })
-    )
+    .input(z.object({ id: z.number() }))
     .query(async ({ input }) => {
       const api = new PokemonClient();
       const pokemon = await api.getPokemonById(input.id);
       return { name: pokemon.name, sprites: pokemon.sprites };
+    }),
+  "cast-vote": procedure
+    .input(z.object({ votedFor: z.number(), votedAgainst: z.number() }))
+    .mutation(async ({ input }) => {
+      const voteInDB = await prisma.vote.create({
+        data: {
+          ...input,
+        },
+      });
+      return { success: true, vote: voteInDB };
     }),
 });
 
